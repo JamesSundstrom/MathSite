5649451c3e418e05baf298c64f5b6f8d9f988345
JamesSundstrom <james.sundstrom@baruch.cuny.edu>
2025-07-26 14:10:40 -0400

    Create Problem class

--- problems/models.py
+++ problems/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Problem(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.IntegerField()
+
+    def __str__(self):
+        return self.question
+    
+    def check_answer(self, answer):
+        return answer == self.answer
