9836083a00fa1f12889cc2af64b54f6d5b690916
JamesSundstrom <james.sundstrom@baruch.cuny.edu>
2025-07-26 22:38:41 -0400

    Add problems view

--- problems/views.py
+++ problems/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
+from random import choice
 
+from .models import Problem
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the problems index.")
+    template = loader.get_template("problems/index.html")
+    context = {"problem": choice(list(Problem.objects.all()))}
+    return HttpResponse(template.render(context, request))
