de7ce51923a6e4a57f9ce9e700c23ec8b1385b31
JamesSundstrom <james.sundstrom@baruch.cuny.edu>
2025-07-26 22:42:03 -0400

    Change problems view URL

--- MathSite/urls.py
+++ MathSite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("problems/", include("problems.urls")),
+    path("", include("problems.urls")),
     path("admin/", admin.site.urls),
 ]
