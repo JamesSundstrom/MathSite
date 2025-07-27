7701b2e9bb6f814946133f7e6f8ecb7339633b4c
JamesSundstrom <james.sundstrom@baruch.cuny.edu>
2025-07-27 04:05:22 -0400

    Allow user to answer the problem

--- problems/urls.py
+++ problems/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "problems"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:problem_id>/answer", views.answer, name="answer"),
+    path("correct", views.correct, name="correct"),
+    path("incorrect", views.incorrect, name="incorrect"),
 ]
--- problems/views.py
+++ problems/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from random import choice
 
 from .models import Problem
@@ -8,3 +10,31 @@ def index(request):
     template = loader.get_template("problems/index.html")
     context = {"problem": choice(list(Problem.objects.all()))}
     return HttpResponse(template.render(context, request))
+
+def correct(request):
+    template = loader.get_template("problems/correct.html")
+    return HttpResponse(template.render({}, request))
+
+def incorrect(request):
+    template = loader.get_template("problems/incorrect.html")
+    return HttpResponse(template.render({}, request))
+
+def answer(request, problem_id):
+    problem = get_object_or_404(Problem, pk=problem_id)
+    try:
+        answer = int(request.POST["answer"])
+    except KeyError:
+        # Redisplay the problem.
+        return render(
+            request,
+            "problems/index.html",
+            {
+                "problem": problem,
+                "error_message": "You didn't enter an answer.",
+            },
+        )
+    else:
+        if problem.check_answer(answer):
+            return HttpResponseRedirect(reverse("problems:correct"))
+        else:
+            return HttpResponseRedirect(reverse("problems:incorrect"))
