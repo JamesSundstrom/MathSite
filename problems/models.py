104682f0e4f40e48a4e3eb6c7ba7400cd779fa14
JamesSundstrom <james.sundstrom@baruch.cuny.edu>
2025-08-02 18:46:36 -0400

    Add "type" field to Problem class

--- problems/models.py
+++ problems/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Problem(models.Model):
     question = models.CharField(max_length=200)
     answer = models.IntegerField()
+    type = models.CharField(max_length=50)
 
     def __str__(self):
         return self.question
+    
+    def type(self):
+        return self.type
     
     def check_answer(self, answer):
         return answer == self.answer
