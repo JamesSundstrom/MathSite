817d970de669bf5766eecd1d5e8a60c24869c00d
JamesSundstrom <james.sundstrom@baruch.cuny.edu>
2025-08-31 15:36:12 -0400

    Add test for evaluation problems

--- problems/tests.py
+++ problems/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Problem
 
-# Create your tests here.
+class EvaluationProblemTests(TestCase):
+    def test_evaluation_problems(self):
+        """
+        Check that every question in the database that asks the user to
+        evaluate a given expression has the correct answer.
+        """
+        for problem in Problem.objects.all():
+            try:
+                correct_answer = eval(problem.question)
+                self.assertEqual(correct_answer, problem.answer)
+            except SyntaxError:
+                continue # Problem is not an "evaluate the expression" problem
+            
